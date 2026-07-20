eb817d492a3cd46569e792e297d9acd662953e2f
mwrosis <kyokosupremacyy@gmail.com>
2026-07-20 19:11:26 -0300

    Add YouTube iframe for autoplaying music

--- js/music.js
+++ js/music.js
@@ -1,1 +1,7 @@
-
+<iframe
+    width="0"
+    height="0"
+    src="https://www.youtube.com/embed/v6WsR4C305w?autoplay=1&mute=1&loop=1&playlist=ID_DEL_VIDEO"
+    frameborder="0"
+    allow="autoplay"
+></iframe>
