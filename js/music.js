ae894a973ffa9f6d1b6f41e3ab69eaefeac51f17
mwrosis <kyokosupremacyy@gmail.com>
2026-07-20 19:11:50 -0300

    Add button to initiate music playback
    Replaced the YouTube iframe with a button to start music playback dynamically.

--- js/music.js
+++ js/music.js
@@ -1,7 +1,16 @@
-<iframe
-    width="0"
-    height="0"
-    src="https://www.youtube.com/embed/v6WsR4C305w?autoplay=1&mute=1&loop=1&playlist=ID_DEL_VIDEO"
-    frameborder="0"
-    allow="autoplay"
-></iframe>
+<button onclick="iniciarMusica()">Entrar</button>
+
+<div id="player"></div>
+
+<script>
+function iniciarMusica() {
+    document.getElementById("player").innerHTML = `
+        <iframe
+            width="0"
+            height="0"
+            src="https://www.youtube.com/embed/ID_DEL_VIDEO?autoplay=1&loop=1&playlist=ID_DEL_VIDEO"
+            allow="autoplay"
+            frameborder="0">
+        </iframe>`;
+}
+</script>
